8cbc06c635d40b7d2540260353fffb0af6499eeb
Mariion-62 <mariongrolleau62@gmail.com>
2024-04-05 09:07:35 +0200

    feat: add page 404, and card benevole

--- app/(default)/mon-parcours/ui/Parcours.tsx
+++ app/(default)/mon-parcours/ui/Parcours.tsx
@@ -7,11 +7,12 @@ import {
 } from "@/components/CardParcours/CardParcours";
 import { useState, useEffect } from "react";
 import styles from "./parcours.module.scss";
-import { Description } from "@/components/Description/Description";
+import { CardBenevole, CardBenevoleProps, dataBenevole } from "@/components/CardBenevole/CardBenevole";
 
 export type DataParcoursType = CardParcoursProps;
+export type DataBenevoleType = CardBenevoleProps;
 
-export default function Parcours() {
+ export default function Parcours() {
   const [allParcours, setAllParcours] = useState<DataParcoursType[]>([]);
   useEffect(() => {
     const dataParcoursCopy: CardParcoursProps[] = [...dataParcours];
@@ -24,29 +25,47 @@ export default function Parcours() {
 
     setAllParcours(dataParcoursCopy);
   }, []);
+
+  const [benevoles, setBenevoles] = useState<CardBenevoleProps[]>([]);
+  useEffect(() => {
+    setBenevoles(dataBenevole);
+  }, []);
+
   return (
     <>
       <h2 className={styles.titleExpPro}>Expériences professionnelles</h2>
       <div className={styles.parcours}>
-        {allParcours.map((oneParcours, index) => {
-          return (
-            <div className={styles.cardParcours} key={index}>
-              <CardParcours
-                id={oneParcours.id}
-                dateBegin={oneParcours.dateBegin}
-                dateEnd={oneParcours.dateEnd}
-                job={oneParcours.job}
-                enterprise={oneParcours.enterprise}
-                experience={oneParcours.experience}
-                picture={oneParcours.picture}
-                expTwo={oneParcours.expTwo}
-                expThree={oneParcours.expThree}
-                expFour={oneParcours.expFour}
-                expFive={oneParcours.expFive}
-              />
-            </div>
-          );
-        })}
+        {allParcours.map((oneParcours, index) => (
+          <div className={styles.cardParcours} key={index}>
+            <CardParcours
+              id={oneParcours.id}
+              dateBegin={oneParcours.dateBegin}
+              dateEnd={oneParcours.dateEnd}
+              job={oneParcours.job}
+              enterprise={oneParcours.enterprise}
+              experience={oneParcours.experience}
+              picture={oneParcours.picture}
+              expTwo={oneParcours.expTwo}
+              expThree={oneParcours.expThree}
+              expFour={oneParcours.expFour}
+              expFive={oneParcours.expFive}
+            />
+          </div>
+        ))}
+      </div>
+      <div>
+        {benevoles.map((benevole, index) => (
+          <div className="cardBenevole" key={index}>
+            <CardBenevole
+              picture={benevole.picture}
+              listOne={benevole.listOne}
+              listTwo={benevole.listTwo}
+              listThree={benevole.listThree}
+              listFour={benevole.listFour}
+              listFive={benevole.listFive}
+            />
+          </div>
+        ))}
       </div>
     </>
   );
