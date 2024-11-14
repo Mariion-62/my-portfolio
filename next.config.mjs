137a95879bc063895a83cf9e30bf9ed33d8932ee
Mariion-62 <mariongrolleau62@gmail.com>
2024-11-14 23:11:31 +0100

    fix: add output

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,10 @@ const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Configure l'export statique
-  i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en',
-    localeDetection: true // Détection automatique de la langue du navigateur
-  }
+  output: 'standalone'
 }
 
 export default withNextIntl(nextConfig)
-
 export const i18n = {
   locales: ['en', 'fr'],
   defaultLocale: 'en'
